fd514cd1da705236f0073317645b188258f92564
pavel.sher <pavel.sher@gmail.com>
2015-12-17 21:55:54 +0100

    one more test

--- vcsHostingLinks-server/src/test/java/org.jetbrains.teamcity.vcsHostingLinks.github/GitHubUrlTest.java
+++ vcsHostingLinks-server/src/test/java/org.jetbrains.teamcity.vcsHostingLinks.github/GitHubUrlTest.java
@@ -5,8 +5,17 @@ import org.testng.annotations.Test;
 
 @Test
 public class GitHubUrlTest {
-  public void test_parse() {
+  public void test_parse_https() {
     GitHubUrl parsed = GitHubUrl.parse("https://github.com/owner/repo.git");
     Assert.assertNotNull(parsed);
+    Assert.assertEquals(parsed.getOwner(), "owner");
+    Assert.assertEquals(parsed.getRepositoryName(), "repo");
+  }
+
+  public void test_parse_ssh() {
+    GitHubUrl parsed = GitHubUrl.parse("git@github.com:JetBrains/teamcity-achievements.git");
+    Assert.assertNotNull(parsed);
+    Assert.assertEquals(parsed.getOwner(), "JetBrains");
+    Assert.assertEquals(parsed.getRepositoryName(), "teamcity-achievements");
   }
 }
